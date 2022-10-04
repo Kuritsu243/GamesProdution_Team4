c93fd04e2a6bf1b1d2fa8ffb1f0a03acabb50c06
Kuritsu <chazza.phone@gmail.com>
2022-10-04 16:45:36 +0100

    Reupload of Box prefab, confirmed working

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -7,7 +7,7 @@ public class playerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Player Controller");
     }
 
     // Update is called once per frame

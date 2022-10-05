de6a61456603e29e9a2f5a8cd4beac3b76d6b7f3
Kuritsu <chazza.phone@gmail.com>
2022-10-05 16:57:31 +0100

    Basic Saturation Mechanic Implementation

---
+++ Assets/Scripts/gameSaturationModifier.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
+
+
+public class gameSaturationModifier : MonoBehaviour
+{
+
+    private GameObject _postProcessController;
+    private PostProcessVolume _postProcessVolume;
+    private ColorGrading _colorGrading;
+    [Range(-100, 100f)]
+    [SerializeField] private int saturation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _postProcessController = GameObject.FindGameObjectWithTag("postProcessingController");
+        _postProcessVolume = _postProcessController.GetComponent<PostProcessVolume>();
+        _postProcessVolume.profile.TryGetSettings(out _colorGrading);
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        _colorGrading.saturation.value = saturation;
+    }
+}
